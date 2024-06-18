cad47e8dc5a801362638b11f01fc9839a0d0d925
v.buchnev <valentinbuchnev0904@gmail.com>
2024-06-18 09:48:20 +0800

    Edit goskey logo

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,12 +5,12 @@
 <template>
   <nav class="navbar bg-body-tertiary">
     <div class="container d-flex justify-content-center">
-      <a class="navbar-brand d-inline-block m-0" href="https://www.mfc38.ru/">
+      <a class="navbar-brand d-inline-block m-0" href="https://www.mfc38.ru/" target="_blank">
         <img src="../assets/logo.svg" alt="МФЦ" height="50">
       </a>
       <div class="vr mx-4"></div>
       <a class="navbar-brand d-inline-block m-0" href="#">
-        <img src="../assets/goskey.png" alt="МФЦ" height="50">
+        <img src="../assets/goskey.svg" alt="МФЦ" height="50">
       </a>
     </div>
   </nav>
